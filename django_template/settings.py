08ae04cb32bdeaad4ca301e498239ed9fdc9e8d0
Behzad Khalifeh <bkhalifeh@protonmail.com>
2025-02-24 18:25:51 +0330

    feat: add database url

--- django_template/settings.py
+++ django_template/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from environ import Env
 from base64 import b64decode
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,14 +85,7 @@ WSGI_APPLICATION = 'django_template.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypass',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
 }
 
 
